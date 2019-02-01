3717f0ae63d45de5aed4c1eebaea2cf865623e55
Aris Alexopoulos <38316559+AlMag1@users.noreply.github.com>
2019-02-01 10:48:47 +0200

    Add files via upload

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeContainer from "../../containers/Home_Container";
+
+const Home = () => {
+  return (
+    <div>
+      <HomeContainer />
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+
+import reducers from "./reducers";
+import Routes from "./routes";
+
+const store = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={store(reducers)}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
